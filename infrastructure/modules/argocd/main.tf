b8efba8e3b705816da37e364802683822f94c543
Developer <developer@disaster-relief.dev>
2026-03-10 22:47:11 +0530

    Updated the postgressql kv sync error

--- infrastructure/modules/argocd/main.tf
+++ infrastructure/modules/argocd/main.tf
@@ -25,45 +25,5 @@ resource "helm_release" "argocd" {
   }
 }
 
-resource "kubernetes_manifest" "postgres_kv_sync" {
-  manifest = {
-    apiVersion = "secrets-store.csi.x-k8s.io/v1"
-    kind       = "SecretProviderClass"
-    metadata = {
-      name      = "postgres-kv-sync"
-      namespace = "default"
-    }
-    spec = {
-      provider = "azure"
-      parameters = {
-        usePodIdentity         = "false"
-        useVMManagedIdentity   = "true"
-        userAssignedIdentityID = var.kubelet_identity_client_id
-        keyvaultName           = var.key_vault_name
-        cloudName              = ""
-        tenantId               = var.tenant_id
-        objects                = <<EOF
-array:
-  - |
-    objectName: postgres-password
-    objectType: secret
-    objectVersion: ""
-EOF
-      }
-      secretObjects = [
-        {
-          secretName = "postgres-secret"
-          type       = "Opaque"
-          data = [
-            {
-              key        = "POSTGRES_PASSWORD"
-              objectName = "postgres-password"
-            }
-          ]
-        }
-      ]
-    }
-  }
-  
-  depends_on = [helm_release.argocd]
+
 }
